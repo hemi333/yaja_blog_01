c9cc9e6aaf8231bff7d0b668ab1dbf861255a4f3
hemi333 <71807433+hemi333@users.noreply.github.com>
2022-08-17 15:03:48 +0900

    외래키 설정

--- models/comment.js
+++ models/comment.js
@@ -10,6 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      this.belongsTo(models.Post, {
+        foreignKey: 'postId',
+        targetKey: 'postId',
+      })
       // define association here
     }
   }
